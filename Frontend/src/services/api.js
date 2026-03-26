721eab3477baf413739ee0431b1ef45550f29e4d
Mauricio Petri Junior <mauricio.petrijr@gmail.com>
2026-03-26 13:53:15 -0300

    config: conectar frontend com backend railway

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend.up.railway.app"
+  baseURL: "https://almoxarifado-dashboard-production.up.railway.app"
 });
 
 export default api;
