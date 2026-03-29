240b1a41a67cb0b968d33f690f6e3c3fb724cbc6
Mauricio Petri Junior <mauricio.petrijr@gmail.com>
2026-03-29 14:21:54 -0300

    update - esconder botão registrar entrada 2

--- Frontend/src/pages/Entradas.jsx
+++ Frontend/src/pages/Entradas.jsx
@@ -1,12 +1,21 @@
-    import EntradaProduto from "../components/EntradaProduto";
+import EntradaProduto from "../components/EntradaProduto";
 
 function Entradas(){
+
+  const token = localStorage.getItem("token");
 
   return(
 
     <div>
 
-      <h1>Entradas de estoque</h1>
+      <h1>📥 Entradas de estoque</h1>
+
+      {/* 🔐 aviso visitante */}
+      {!token && (
+        <p style={{ color: "orange" }}>
+          Você está em modo visitante. Faça login para registrar entradas.
+        </p>
+      )}
 
       <EntradaProduto />
 
